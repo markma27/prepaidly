cdf7ef321ba2f35584af083c9e73d8a6556d7187
edmondhuo <edmond.huo@prepaidly.io>
2026-02-21 17:34:34 -0800

    Removed unnecessary sql file.

--- database/migration_remove_unused_supabase_columns.sql
+++
@@ -1,8 +0,0 @@
--- Remove unused Supabase columns from users
-ALTER TABLE users
-DROP COLUMN IF EXISTS supabase_phone,
-DROP COLUMN IF EXISTS supabase_updated_at,
-DROP COLUMN IF EXISTS supabase_last_sign_in_at,
-DROP COLUMN IF EXISTS supabase_email_confirmed_at,
-DROP COLUMN IF EXISTS supabase_phone_confirmed_at,
-DROP COLUMN IF EXISTS supabase_raw_json;
